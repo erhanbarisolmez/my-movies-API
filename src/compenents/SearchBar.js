a004bdb8a6b37570a94079ac46ace3855d7dbdb2
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2022-12-28 19:06:54 +0300

    Add files via upload

---
+++ src/compenents/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+
+   handleForSubmit = (event) =>{
+    event.preventDefault()
+   }
+    render(){
+   
+        return(
+            <form onSubmit={this.handleForSubmit}>
+            <div className="form-row mb-5">
+                <div className="col-12">
+                    <input
+                     onChange={this.props.searchMovieProp} 
+                     type="text" className="form-control"
+                     placeholder="Seach a movie"
+                     
+                    />
+                </div>
+            </div>  
+        </form>
+          )
+        
+    }
+}
+
+export default SearchBar;
